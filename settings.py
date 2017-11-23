6673b111ab8d93571a8bb8bb146891c70749ba7f
boflops <florisbouman1@gmail.com>
2017-11-23 00:51:32 -0400

    eth

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-CURRENCY = 'btc' ##change currency pair options are: btc, ltc, eth, and all are matched with _cad
+CURRENCY = 'eth' ##change currency pair options are: btc, ltc, eth, and all are matched with _cad
 TRIGGER_DIFFERENCE = 1.001 ##how much under or over the market price a quadriga order should be for the bot to pouch it 1.01 = 1%, 1.005 = 0.5% etc
 API_KEY = 'api_key'
 API_SECRET = 'api_secret'
