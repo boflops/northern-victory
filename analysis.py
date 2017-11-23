e0548e387d01802eddb2121f589180973465d616
boflops <florisbouman1@gmail.com>
2017-11-22 22:26:32 -0400

    add exponential moving average

--- analysis.py
+++ analysis.py
@@ -18,8 +18,32 @@ class History:
             print("program hasnt been running long enough to get this MA")
             return None
         ##slice list to include only the prices from end of the list for the specified no of minutes
-        fiveminlist = self.recentprices[len(self.recentprices) - (periods):len(self.recentprices)]
-        return sum(fiveminlist) / periods ##return MA over this period
+        sublist = self.recentprices[len(self.recentprices) - (periods):len(self.recentprices)]
+        return sum(sublist) / periods ##return MA over this period
+
+    def getExpMovingAverageForMinutes(self, minutes):
+
+        ##convert minutes into 10 second periods
+        periods = minutes * 6
+        if(len(self.recentprices) < int(periods)): ##havent been running long enough to get this data
+            print("program hasnt been running long enough to get this EMA")
+            return None
+
+        ##slice list to include only the prices from end of the list for the specified no of minutes
+        sublist = self.recentprices[len(self.recentprices) - (periods):len(self.recentprices)]
+
+        ##expMA formula:
+        ema = 0
+        top = 0
+        bot = 0
+        for i in range (len(sublist)):
+            print(i)
+            top += sublist[i] * (1 - a(i))**(i - 1)
+            bot += (1- a(i))**(i - 1)
+        return top / bot ##return EMA over this period
+
+def a(n):
+    return (2/(n+1))
 
 def checkIfGoodDeal(availablePrice, indexedPrice, bull):
     if(bull):
